4a381a9d7474131446e20a9bfd3a9167f48c6bf4
Arun Kumar #Ak97 <arun.gutala@gmail.com>
2019-12-19 12:20:39 +0530

    Add files via upload

--- foodstore.js
+++ foodstore.js
@@ -1,15 +1,39 @@
 var xmlHttp = createXmlHttpRequestObject();
 
 function createXmlHttpRequestObject(){
-	xmlHttp = new xmlHttpRequest();
-}	
+	var xmlHttp;
+	
+	if(window.ActiveXObject){
+		try{
+			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}catch(e){
+			xmlHttp = false;
+		}
+	}
+	else{
+		try{
+			xmlHttp = new xmlHttpRequest();
+		}catch(e){
+			xmlHttp = false;
+		}
+	}
+	
+	if(!xmlHttp)
+		alert("Can't create that object!");
+	else
+		return xmlHttp;
+}
 
 function process(){
-
+	if(xmlHttp.readyState==0 || xmlHttp.readyState==4){
 		food = encodeURIComponent(document.getElementById("userinput").value);
 		xmlHttp.open("GET", "foodstore.php?food=" + food, true);
 		xmlHttp.onreadystatechange = handleServerResponse;
 		xmlHttp.send(null);
+	}
+	else{
+		setTimeout('process()',1000);
+	}
 }
 
 function handleServerResponse(){
